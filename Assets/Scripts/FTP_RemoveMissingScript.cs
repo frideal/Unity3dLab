f99daea81c5460f9bda4c8082cdddb1629b8905d
Frideal <yankun007@gmail.com>
2018-07-18 15:53:38 +0800

    - Add Replacement script.

---
+++ Assets/Scripts/FTP_RemoveMissingScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace FastToolsPackage
+{
+    public class FTP_RemoveMissingScript
+    {
+        [MenuItem("FTP_Tools/Remove Missing Script")]
+        static void RemoveMissingScript()
+        {
+            GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+            foreach (GameObject gameObject in allObjects)
+            {
+                var components = gameObject.GetComponents<Component>();
+                var serializedObject = new SerializedObject(gameObject);
+                var prop = serializedObject.FindProperty("m_Component");
+                int r = 0;
+                for (int j = 0; j < components.Length; j++)
+                {
+                    if (components[j] == null)
+                    {
+                        EditorSceneManager.MarkSceneDirty(gameObject.scene);
+                        prop.DeleteArrayElementAtIndex(j - r);
+                        r++;
+                    }
+                }
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/FTP_ReplacementSelected.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+//
+// http://wiki.unity3d.com/index.php/ReplaceSelection
+//
+namespace FastToolsPackage
+{
+    public class FTP_ReplacementSelected : ScriptableWizard
+    {
+        static GameObject s_replacement;
+        static bool s_keepOriginals = false;
+
+        public GameObject m_replacement = null;
+        public bool m_keepOriginals = false;
+
+        [MenuItem("FTP_Tools/Replace Selected... _%#R")]
+        static void CreateWizard()
+        {
+            ScriptableWizard.DisplayWizard("Replacement Selection", typeof(FTP_ReplacementSelected), "Replacement");
+        }
+
+        public FTP_ReplacementSelected()
+        {
+            m_replacement = s_replacement;
+            m_keepOriginals = s_keepOriginals;
+        }
+
+        private void OnWizardUpdate()
+        {
+            s_replacement = m_replacement;
+            s_keepOriginals = m_keepOriginals;
+        }
+
+        private void OnWizardCreate()
+        {
+            if (m_replacement == null)
+                return;
+
+            Transform[] targets = Selection.GetTransforms(SelectionMode.OnlyUserModifiable);
+
+            for (int i = 0; i < targets.Length; i++)
+            {
+                GameObject newObj;
+                PrefabType type = PrefabUtility.GetPrefabType(m_replacement);
+                if (type == PrefabType.Prefab || type == PrefabType.ModelPrefab)
+                {
+                    newObj = PrefabUtility.InstantiatePrefab(m_replacement) as GameObject;
+                }
+                else
+                {
+                    newObj = Editor.Instantiate(m_replacement) as GameObject;
+                }
+
+                Transform newTrs = newObj.transform;
+                newTrs.parent = targets[i].parent;
+                newTrs.name = m_replacement.name;
+                newTrs.localPosition = targets[i].localPosition;
+                newTrs.localScale = targets[i].localScale;
+                newTrs.localRotation = targets[i].localRotation;
+
+                Undo.RegisterCreatedObjectUndo(newObj, "Undo Replacement");
+            }
+             
+            if (!m_keepOriginals)
+            {
+                foreach (GameObject go in Selection.gameObjects)
+                {
+                    if (go != null)
+                        Undo.DestroyObjectImmediate(go);
+                }
+            }
+        }
+    }
+}
